aa5176ccb42cd5985d5f541d70510fd6d1c74a56
ypyp66 <ypyp66@naver.com>
2020-12-02 04:52:39 +0900

    useMemo 시작

---
+++ Chapter8/hooks/src/Average.js
@@ -0,0 +1,21 @@
+const { useState } = require("react");
+
+const getAverage = (numbers) => {
+  console.log("평균값 계산중...");
+  if (numbers.length === 0) return 0;
+  const sum = numbers.reduce((a, b) => a + b);
+  return sum / numbers.length;
+};
+
+const Average = () => {
+  const [list, setList] = useState([]);
+  const [number, setNumber] = useState("");
+
+  const onChange = (e) => {
+    setNumber(e.target.value);
+  };
+
+  const onInsert = (e) => {
+    const nextList = list.concat(parseInt(number));
+  };
+};
--- Chapter8/hooks/src/Info.js
+++ Chapter8/hooks/src/Info.js
@@ -1,33 +1,39 @@
-import { useEffect, useState } from "react";
+import { useEffect, useReducer, useState } from "react";
 
-const Info = () => {
-  const [name, setName] = useState("");
-  const [nickname, setNickName] = useState("");
-
-  useEffect(() => {
-    console.log("useEffect 렌더링 완료");
-    console.log(name);
-    return () => {
-      console.log("clean up");
-      console.log(name);
-    };
-  }, []);
-
-  const onChangeName = (e) => {
-    setName(e.target.value);
+function reducer(state, action) {
+  console.log("reducer");
+  console.log(action);
+  console.log(state);
+  return {
+    ...state, //기존 값 복사
+    [action.name]: action.value,
   };
+}
+const Info = () => {
+  const [state, dispatch] = useReducer(reducer, {
+    name: "",
+    nickname: "",
+  });
+  const { name, nickname } = state;
 
-  const onChangeNickname = (e) => {
-    setNickName(e.target.value);
+  const onChange = (e) => {
+    console.log("onChange");
+    dispatch(e.target);
   };
 
   return (
     <div>
       <div>
-        <input value={name} onChange={onChangeName} placeholder="name"></input>
         <input
+          name="name"
+          value={name}
+          onChange={onChange}
+          placeholder="name"
+        ></input>
+        <input
+          name="nickname"
           value={nickname}
-          onChange={onChangeNickname}
+          onChange={onChange}
           placeholder="nickname"
         ></input>
       </div>
