bf64b3203fc35452935a1d77a33daac0be8cbb93
ypyp66 <ypyp66@naver.com>
2021-01-06 02:35:34 +0900

    spa알아보기 및 route, link

---
+++ Chapter13/spa/src/About.js
@@ -0,0 +1,9 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>About me</p>
+    </div>
+  );
+};
+export default About;
--- Chapter13/spa/src/App.js
+++ Chapter13/spa/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Link } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Profile from "./Profile";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ margin: "1rem" }}>
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+        <li>
+          <Link to="/profile/velopert">velopert 프로필</Link>
+        </li>
+        <li>
+          <Link to="/profile/gildong">gildong 프로필</Link>
+        </li>
+      </ul>
+      <hr />
+      <Route path="/" component={Home} exact={true} />
+      <Route path={["/about", "/info"]} component={About} />
+      <Route path="/profile/:username" component={Profile} />
     </div>
   );
 }
