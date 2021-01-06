491dba807f6ac5b8da5b201dd120cf63753ae400
ypyp66 <ypyp66@naver.com>
2021-01-07 00:04:59 +0900

    url 쿼리

--- Chapter13/spa/src/About.js
+++ Chapter13/spa/src/About.js
@@ -1,8 +1,15 @@
-const About = () => {
+import qs from "qs";
+
+const About = ({ location }) => {
+  const query = qs.parse(location.search, {
+    ignoreQueryPrefix: true, //이 설정을 통해 문자열 맨 앞의 ?를 생략해줌
+  });
+  const showDetail = query.detail === "true";
   return (
     <div>
       <h1>About</h1>
       <p>About me</p>
+      {showDetail && <p>Detail is true</p>}
     </div>
   );
 };
